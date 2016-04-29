af7cddefeb1b2911a7001c48aa77b33a9611c3bc
caulkerman <ih8blackwidowspiders@yahoo.com>
2016-04-28 20:06:42 -0600

    connected app to mLabs, created gitignore password file

---
+++ api/mLabsPassword/mLabsPassword.js
@@ -0,0 +1,3 @@
+module.exports = {
+	password: "mongodb://randy:earlobe1@ds021701.mlab.com:21701/the-randomizer"
+}
--- server.js
+++ server.js
@@ -30,8 +30,9 @@ app.delete('/api/subject-items-Lists/:id', RandomCtrl.delete);
 
 // Connections
 var port = 9898;
-var mongoUri = 'mongodb://localhost:27017/the-randomizer';
-// var mongoUri = "mongodb://randy:earlobe1@ds033915.mongolab.com:33915/randystory"
+// var mongoUri = 'mongodb://localhost:27017/the-randomizer';
+var mLabsPassword = require("./api/mLabsPassword/mLabsPassword")
+var mongoUri = mLabsPassword.password;
 mongoose.set('debug', true);
 mongoose.connect(mongoUri);
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
